f7b81bce83196f197ae9719d1521a84e261aba4a
sanjayaharshana <sanjayaharasana@gmail.com>
2021-07-12 20:08:54 +0530

    App Studio Function

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Food Studio</title>
+    <title>@yield('title')</title>
     <link rel="stylesheet" type="text/css" href="{{url('css/bootstrap.min.css')}}">
     <link rel="stylesheet" type="text/css" href="{{url('css/style.css')}}">
     <link rel="icon" type="image/png" href="{{url('img/logo-fs.png')}}" sizes="32x32"/>
